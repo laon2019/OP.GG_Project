de8e4319d9156b7ee969cf26e739b94e2876aa4d
laon2019 <laon5987@nate.com>
2024-01-02 21:04:12 +0900

    테스트

--- my-app/src/redux/actions/userAction.js
+++ my-app/src/redux/actions/userAction.js
@@ -4,12 +4,12 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const getUserInfo = createAsyncThunk("summoners", async (userNickName, { rejectWithValue }) => {
     const API_KEY = process.env.REACT_APP_API_KEY;
     try {
-        const userInfoApi = await api.get(`kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userNickName}?api_key=RGAPI-665f99aa-eeda-4b5f-a36f-2e76f584c44b`);
-        const leagueInfoApi = await api.get(`kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${userInfoApi.data.id}?api_key=RGAPI-665f99aa-eeda-4b5f-a36f-2e76f584c44b`);
-        const matchInfoApi = await api.get(`asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${userInfoApi.data.puuid}/ids?start=0&count=12&api_key=RGAPI-665f99aa-eeda-4b5f-a36f-2e76f584c44b`);
+        const userInfoApi = await api.get(`kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userNickName}?api_key=${API_KEY}`);
+        const leagueInfoApi = await api.get(`kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${userInfoApi.data.id}?api_key=${API_KEY}`);
+        const matchInfoApi = await api.get(`asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${userInfoApi.data.puuid}/ids?start=0&count=12&api_key=${API_KEY}`);
 
         const matchDetailPromises = matchInfoApi.data.map(async (matchId) => {
-            const response = await api.get(`asia.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=RGAPI-665f99aa-eeda-4b5f-a36f-2e76f584c44b`);
+            const response = await api.get(`asia.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${API_KEY}`);
             return response.data;
         });
         
@@ -33,12 +33,12 @@ export const getAdditionalMatchDetails = createAsyncThunk(
     async ({ userPuuid, start, count }, { rejectWithValue }) => {
       try {
         const matchInfoApi = await api.get(
-          `asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${userPuuid}/ids?start=${start}&count=${count}&api_key=RGAPI-665f99aa-eeda-4b5f-a36f-2e76f584c44b`
+          `asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${userPuuid}/ids?start=${start}&count=${count}&api_key=${API_KEY}`
         );
   
         const matchDetailPromises = matchInfoApi.data.map(async (matchId) => {
           const response = await api.get(
-            `asia.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=RGAPI-665f99aa-eeda-4b5f-a36f-2e76f584c44b`
+            `asia.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${API_KEY}`
           );
           return response.data;
         });
